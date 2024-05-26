f28f9817b8ead48f85177d990e10140990ba362a
anthonyych4n <anthony168c@gmail.com>
2024-05-25 20:39:00 -0600

    Update LandingPage.jsx

--- taskmanager/src/app/LandingPage/LandingPage.jsx
+++ taskmanager/src/app/LandingPage/LandingPage.jsx
@@ -1,28 +1,13 @@
 import "./LandingPage.css";
 // importing components from react-router-dom package
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-} from "react-router-dom";
- 
-import LogIn from "./app/LoginPage/LoginPage";
-import CreateAccount from "./app/CreateAccountPage/CreateAccountPage.jsx";
  
 function LandingPage() {
     return (
-        <Router>
-            <div className="landing-page">
-                <Switch>
-                    <Route path="/login">
-                        <LogIn />
-                    </Route>
-                    <Route path="/create-account">
-                        <CreateAccount />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+        <div>
+            <h1>Shibal Gaesakki Task Manager</h1>
+            <button>Login</button>
+            <button>Create Account</button>
+        </div>
     );
 }
  
